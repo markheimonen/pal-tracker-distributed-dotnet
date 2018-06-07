c40b759e902243db00031e46e91c3c0f22ba1fd6
Mark Heimonen <mheimonen@west.com>
2018-06-07 13:36:37 -0600

    Hystrix Circuit Breaker Implementation

--- Applications/BacklogServer/Startup.cs
+++ Applications/BacklogServer/Startup.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Logging;
 using Steeltoe.CloudFoundry.Connector.MySql.EFCore;
 using Pivotal.Discovery.Client;
 using Steeltoe.Common.Discovery;
+using Steeltoe.CircuitBreaker.Hystrix;
+
 
 namespace BacklogServer
 {
@@ -38,8 +40,11 @@ namespace BacklogServer
                     BaseAddress = new Uri(Configuration.GetValue<string>("REGISTRATION_SERVER_ENDPOINT"))
                 };
 
-                return new ProjectClient(httpClient);
+                var logger = sp.GetService<ILogger<ProjectClient>>();
+                return new ProjectClient(httpClient, logger);
             });
+
+            services.AddHystrixMetricsStream(Configuration);
 
             services.AddDiscoveryClient(Configuration);
         }
@@ -53,6 +58,9 @@ namespace BacklogServer
             app.UseMvc();
 
             app.UseDiscoveryClient();
+
+            app.UseHystrixMetricsStream();
+            app.UseHystrixRequestContext();
         }
     }
 }
--- Components/Backlog/ProjectClient.cs
+++ Components/Backlog/ProjectClient.cs
@@ -1,25 +1,49 @@
-﻿using System.Net.Http;
+﻿using System.Collections.Generic;
+using System.Net.Http;
 using System.Runtime.Serialization.Json;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 
 namespace Backlog
 {
     public class ProjectClient : IProjectClient
     {
         private readonly HttpClient _client;
-
-        public ProjectClient(HttpClient client)
+        private Dictionary<long, ProjectInfo> _cache;
+        private readonly ILogger<ProjectClient> _logger;
+       
+        public ProjectClient(HttpClient client, ILogger<ProjectClient> logger)
         {
             _client = client;
+            _cache = new Dictionary<long, ProjectInfo>();
+            _logger = logger;
         }
 
-        public async Task<ProjectInfo> Get(long projectId)
+        public async Task<ProjectInfo> Get(long projectId) =>
+         await new GetProjectCommand(DoGet, DoGetFromCache, projectId).ExecuteAsync();
+
+        public async Task<ProjectInfo> DoGet(long projectId)
         {
             _client.DefaultRequestHeaders.Accept.Clear();
             var streamTask = _client.GetStreamAsync($"project?projectId={projectId}");
+            _logger.LogInformation($"Attempting to fetch projectId: {projectId}");
 
             var serializer = new DataContractJsonSerializer(typeof(ProjectInfo));
-            return serializer.ReadObject(await streamTask) as ProjectInfo;
+            var data = serializer.ReadObject(await streamTask) as ProjectInfo;
+            if (_cache.ContainsKey(projectId))
+            {
+                _cache[projectId] = data;
+            }
+            else 
+            {
+                _cache.Add(projectId, data);
+            }
+            return data;
+        }
+
+        public Task<ProjectInfo> DoGetFromCache(long projectId)
+        {          
+           return Task.FromResult(_cache[projectId]);                 
         }
     }
 }
